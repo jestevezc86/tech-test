c3b015f82d52b7e5a665fea988af0b86ba6e0dcd
jestevezc86 <126386803+jestevezc86@users.noreply.github.com>
2023-02-26 11:52:29 +0100

    Add files via upload

--- app/src/Entity/Province.php
+++ app/src/Entity/Province.php
@@ -39,4 +39,33 @@ class Province
     {
         $this->municipalities = new ArrayCollection();
     }
+
+
+    
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
 }
